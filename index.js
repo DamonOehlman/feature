f7ae3a5537b2e91913d1b01f37ba2b84befc147e
Damon Oehlman <damon.oehlman@sidelab.com>
2013-04-25 19:07:53 +1000

    Tweaked css property accessor test to return an accessor / mutator function

--- index.js
+++ index.js
@@ -2,20 +2,38 @@ var prefixes = ['ms', 'O', 'Moz', 'Webkit'],
     props = {},
     style;
 
+function createGetterSetter(propName) {
+    return function(element, value) {
+        // if we have a value update 
+        if (typeof value != 'undefined') {
+            element.style[propName] = value;
+        }
+
+        return getComputedStyle(element, propName);
+    };
+}
+
 module.exports = function(prop) {
-    var ii, propName;
+    var ii,
+        propName,
+        pascalCaseName;
 
     style = style || document.body.style;
 
     // if we already have a value for the target property, return
     if (props[prop] || style[prop]) return props[prop];
 
+    // convert a dash delimited propertyname (e.g. box-shadow) into pascal cased name (BoxShadow)
+    pascalCaseName = prop.split('-').reduce(function(memo, val) {
+        return memo + val.charAt(0).toUpperCase() + val.slice(1);
+    }, '');
+
     // check for the property
     for (ii = prefixes.length; ii--; ) {
-        propName = prefixes[ii] + prop.charAt(0).toUpperCase() + prop.slice(1);
+        propName = prefixes[ii] + pascalCaseName;
 
         if (typeof style[propName] != 'undefined') {
-            props[prop] = prefixes[ii].toLowerCase();
+            props[prop] = createGetterSetter(propName);
             break;
         }
     }
--- test/style-properties.js
+++ test/style-properties.js
@@ -1,13 +1,15 @@
 var feature = require('../'),
     test = require('tape'),
     props = [
-        'transform'
+        'transform',
+        'box-shadow'
     ];
 
 test('style properties test', function(t) {
-    t.plan(props.length);
+    t.plan(props.length * 2);
 
     props.forEach(function(prop) {
         t.ok(feature(prop), prop + ' property detected ok as expected');
+        t.ok(typeof feature(prop) == 'function', prop + ' has accessor function');
     });
 });
